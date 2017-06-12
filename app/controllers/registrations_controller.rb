0f513bae8abb6de57a38ec76cbdaef6d047cfa25
A. Felipe Cabargas <felipe.cabargas@gmail.com>
2017-06-12 13:08:52 -0400

    Edit: controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
-class RegistrationsController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
   private
   def sign_up_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 
-  def account_update_params
+  def account_update_paramsß
     params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password)
   end
 end
