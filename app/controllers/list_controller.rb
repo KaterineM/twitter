09d6fd866b4fda7de4c32630a208b7484a70d781
KaterineM <katty.dmt@gmail.com>
2017-06-13 17:58:08 -0400

    Add: CRUD to List controller

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,8 +1,19 @@
 class ListController < ApplicationController
   before_action :set_list, only: [:show]
+  before_action :authenticate_user!, only: [:index]
+
+  def index
+    @lists = List.where(user: current_user)
+  end
 
   #/list/id
   def show
+  end
+
+  def new
+  end
+
+  def create
   end
 
   private
