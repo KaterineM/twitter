1a9a17d0160811e8ce812d070af081c68ad6b973
KaterineM <katty.dmt@gmail.com>
2017-06-13 17:54:25 -0400

    Add: as in list routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations'}
   root 'user_profile#show'
 
-  get '/lists/:id', to: 'lists#show'
+  get '/lists/:id', to: 'lists#show', as: 'list'
 end
